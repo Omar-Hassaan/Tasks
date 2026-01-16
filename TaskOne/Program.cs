dd4c44dad71ef09f0530ac8f8eef1d61fae75255
Omar Hassaan <omarhassaann2@gmail.com>
2026-01-16 03:29:11 +0200

    Update TaskOne

---
+++ TaskOne/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int perSmallCharge = 25;
+            int perLargeCharge = 35;
+            const double tax = 0.06;
+            const int estimateDays = 30;
+
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.Write("Enter the number of small carpets: ");
+            int numSmallCarpets = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of large carpets: ");
+            int numLargeCarpets = Convert.ToInt32(Console.ReadLine());
+
+            double TotalWithNoTax = (perSmallCharge * numSmallCarpets) + (perLargeCharge * numLargeCarpets);
+
+            Console.WriteLine($"Price per small Carpet: {perSmallCharge}");
+            Console.WriteLine($"Price per large Carpet: {perLargeCharge}");
+            Console.WriteLine($"Cost: {TotalWithNoTax:C}");
+            Console.WriteLine($"Tax:  {tax * TotalWithNoTax:C}");
+            Console.WriteLine("===============================");
+            Console.WriteLine($"Total estimate: {TotalWithNoTax + (tax * TotalWithNoTax):C}");
+            Console.WriteLine($"This estimate is valid for {estimateDays} days");
+        }
+    }
+}
---
+++ TaskTwo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
